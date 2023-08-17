6b55575a12ce0fe73ed933b73e52eb9af805eac6
xuzepei <xuzepei@gmail.com>
2023-08-17 17:56:44 +0800

    no message

--- my-webapp/client/src/Gallery.js
+++ my-webapp/client/src/Gallery.js
@@ -1,5 +1,6 @@
 
-function Avatar({person, size=100}) {
+
+function Avatar({person, size='70'}) {
     return (
         <img
         className="avatar"
@@ -54,11 +55,30 @@ function Profile({person}) {
   );
 }
 
+function Profile2({ person }) {
+  return (
+    <section className="profile2">
+      <Avatar person={person} size={person.imageSize} />
+    </section>
+  );
+}
+
+function Card({title, children}) {
+  return (
+    <section>
+    <div className="card">
+      <h1>{title}</h1>
+      {children}
+    </div>
+    </section>
+  );
+}
+
 export default function Gallery() {
 
     const person1 = {
         name: "Maria Skłodowska-Curie",
-        imageId: 'szV5sdG',
+        imageId: 'OKS67lh',
         imageSize: 70,
       profession: "physicist and chemist",
       awards:["Nobel Prize in Physics", "Nobel Prize in Chemistry", "Davy Medal", "Matteucci Medal"],
@@ -77,8 +97,15 @@ export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <Profile person={person1}/>
-      <Profile person={person2}/>
+      <Profile person={person1} />
+      <Profile person={person2} />
+      <Card title={"Photo"}>
+        <Profile2 person={person1} />
+      </Card>
+      
+      <Card title={"About"}>
+        <h4>"Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis."</h4>
+      </Card>
     </div>
   );
 }
